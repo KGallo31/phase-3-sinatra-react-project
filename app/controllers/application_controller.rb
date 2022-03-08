049c43216a0526958d64737a9517076fa676d2bd
Jonathan Bryant <spectrum19tech@gmail.com>
2022-03-08 06:10:45 -0800

    Added first Sinatra routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,20 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/rooms' do 
+    rooms = Room.all
+    rooms.to_json
   end
+
+  get '/rooms/:id' do
+    room = Room.find(params[:id])
+    room.to_json
+  end
+
+
+
+  # get "/" do
+  #   { message: "Good luck with your project!" }.to_json
+  # end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ room_types = ["Standard","Deluxe","Presidential suite","penthouse"]
 # end
 
 100.times do |r|
-    Room.create(room_number: r,reserved: false,room_type: room_types.sample,price: 200,hotel_id: Hotel.all.sample.id)
+    Room.create(room_number: r+1,reserved: false,room_type: room_types.sample,price: 200,hotel_id: Hotel.all.sample.id)
 end
 
 
